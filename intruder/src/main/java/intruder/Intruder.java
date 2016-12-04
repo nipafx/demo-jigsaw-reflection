aab45fa04633a9ae2b7249041bf6fc43de8886a2
nipa <nipa@codefx.org>
2016-12-04 21:25:40 +0100

    Try opening the package

--- intruder/src/main/java/intruder/Intruder.java
+++ intruder/src/main/java/intruder/Intruder.java
@@ -1,16 +1,35 @@
 package intruder;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Module;
 
 public class Intruder {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		Class<?> owner = Class.forName("owner.Owner");
+		invokeMethods(owner);
+//		open(owner, "owner");
+//		invokeMethods(owner);
+	}
+
+	private static void invokeMethods(Class<?> owner) {
 		invoke(owner, "public");
 		invoke(owner, "protected");
 		invoke(owner, "default");
 		invoke(owner, "private");
 		System.out.println();
+	}
+
+	private static void open(Class<?> type, String packageName) {
+		Module owningModule = type.getModule();
+		Module intruderModule = Intruder.class.getModule();
+		try {
+			owningModule.addOpens(packageName, intruderModule);
+			System.out.printf("Opened %s/%s to %s.%n", owningModule, packageName, intruderModule);
+		} catch (Exception ex) {
+			System.out.printf(
+					"Could not open %s/%s to %s: %s%n", owningModule, packageName, intruderModule, ex.getMessage());
+		}
 	}
 
 	private static void invoke(Class<?> type, String visibility) {
